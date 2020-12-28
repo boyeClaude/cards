a44bd68758770a4d12532cbe86b07a6790d0d436
boyeClaude <boye.claude@gmail.com>
2020-12-28 17:25:53 +0000

    card component finished and working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,80 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    {
+      title: 'Neat tree',
+      username: 'nature',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/biking.jpeg',
+    },
+
+    {
+      title: 'Snowy Mountain',
+      username: 'John Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/mountain.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+
+    {
+      title: 'Mountain Biking',
+      username: 'Anne Doe',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+  ];
 }
